a76245eda34e5b5a2aeb5fe5d066e8510362304a
RaphaelSiller <raphael.siller123@gmail.com>
2022-11-18 12:19:19 +0100

    worked on main menu.

--- src/net/tfobz/vokabeltrainer/gui/MainMenu.java
+++ src/net/tfobz/vokabeltrainer/gui/MainMenu.java
@@ -5,12 +5,18 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import net.tfobz.vokabeltrainer.model.VokabeltrainerDB;
 
 public class MainMenu extends JFrame {
 	Dimension buttonsize = new Dimension(600, 150);
@@ -19,13 +25,44 @@ public class MainMenu extends JFrame {
 		this.setTitle("Vokableltrainer - MainMenu");
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.getContentPane().setLayout();
+		this.getContentPane().setLayout(new GridLayout(5, 5));
+		
+		// Setzt das Theme auf dem vom Benutzer ausgewählten Theme, (z.B. gtk+, windows, ...)
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JLabel titel = new JLabel("Vokabeltrainer");
+		titel.setHorizontalAlignment(JLabel.CENTER);
 		
 		JButton createLernkartei = new JButton("Neue Lernkartei");
 		createLernkartei.setPreferredSize(buttonsize);
-		JButton viewLernkartein = new JButton("pla Lernkartei");
+		JButton viewLernkartein = new JButton("Lernkarteienübersicht");
 		viewLernkartein.setPreferredSize(buttonsize);
 		
+		for (int i = 0; i < 25; i++) {
+			switch (i) {
+				case 7: {
+					this.add(titel);
+					break;
+				}
+				case 16: {
+					this.add(createLernkartei);
+					break;
+				}
+				case 18: {
+					this.add(viewLernkartein);
+					break;
+				}
+				default:
+					this.add(Box.createRigidArea(buttonsize));
+//					this.add(new JButton(""));
+			}
+		}
 		
 	}
 
