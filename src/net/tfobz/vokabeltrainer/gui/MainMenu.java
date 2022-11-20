71ae2c81de2143941ec0bb78723f32002e18847b
RaphaelSiller <raphael.siller123@gmail.com>
2022-11-20 11:22:29 +0100

    feat<Main Menu>: finished Layout of Main Menu

--- src/net/tfobz/vokabeltrainer/gui/MainMenu.java
+++ src/net/tfobz/vokabeltrainer/gui/MainMenu.java
@@ -6,8 +6,10 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -21,55 +23,55 @@ import javax.swing.UnsupportedLookAndFeelException;
 import net.tfobz.vokabeltrainer.model.VokabeltrainerDB;
 
 public class MainMenu extends JFrame {
-	Dimension buttonsize = new Dimension(600, 150);
-	
+	Dimension buttonsize = new Dimension(300, 75);
+
 	public MainMenu() {
 		this.setTitle("Vokableltrainer - MainMenu");
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setSize(1200, 800); // Default Fenstergroesse, wenn Fenster nicht maximiert ist
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH); // Starte Fenster maximiert
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.getContentPane().setLayout(new GridLayout(5, 5));
-		
-		// Setzt das Theme auf dem vom Benutzer ausgewählten Theme, (z.B. gtk+, windows, ...)
+		this.getContentPane().setLayout(new GridBagLayout());
+
+		// Setzt das Theme auf dem vom Benutzer ausgewählten Theme, (z.B. gtk+, windows,
+		// ...)
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			//Bugfix: Wenn Gtk Theme benutzt wird, dann wird Background nicht benutzt
-			if(UIManager.getSystemLookAndFeelClassName() == "com.sun.java.swing.plaf.gtk.GTKLookAndFeel") {
+			// Bugfix: Wenn Gtk Theme benutzt wird, dann wird Background nicht benutzt
+			if (UIManager.getSystemLookAndFeelClassName() == "com.sun.java.swing.plaf.gtk.GTKLookAndFeel") {
 				this.getContentPane().setBackground(new Color(56, 56, 56));
 			}
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
 				| UnsupportedLookAndFeelException e) {
 			e.printStackTrace();
 		}
-		
+		// Defniere Komponenten
 		JLabel titel = new JLabel("Vokabeltrainer");
-		titel.setFont(new Font("Karumbi", Font.BOLD, 85));
+		titel.setFont(new Font("Karumbi", Font.BOLD, 150));
 		titel.setHorizontalAlignment(JLabel.CENTER);
-		
+
 		JButton createLernkartei = new JButton("Neue Lernkartei");
 		createLernkartei.setPreferredSize(buttonsize);
 		JButton viewLernkartein = new JButton("Lernkarteienübersicht");
 		viewLernkartein.setPreferredSize(buttonsize);
-		
-		for (int i = 0; i < 25; i++) {
-			switch (i) {
-				case 7: {
-					this.add(titel);
-					break;
-				}
-				case 16: {
-					this.add(createLernkartei);
-					break;
-				}
-				case 18: {
-					this.add(viewLernkartein);
-					break;
-				}
-				default:
-					this.add(Box.createRigidArea(buttonsize));
-//					this.add(new JButton(""));
-			}
-		}
-		
+		GridBagConstraints c = new GridBagConstraints();
+
+		// Platziere Komponenten
+		c.gridx = 1;
+		c.gridy = 1;
+		c.gridwidth = 3;
+		this.add(titel, c);
+
+		c.gridx = 1;
+		c.gridy = 3;
+		c.gridwidth = 1;
+		c.insets = new Insets(0, 0, 0, 100);
+		this.add(createLernkartei, c);
+
+		c.gridx = 3;
+		c.gridy = 3;
+		c.gridwidth = 1;
+		c.insets = new Insets(0, 100, 0, 0);
+		this.add(viewLernkartein, c);
 	}
 
 }
