fdb6e6d301b75dfb590774f2bdc73da56aae9928
Flixi05 <felix.gallmetzer@gmail.com>
2022-11-24 09:03:15 +0100

    Started working on CreateLernkartei

--- src/net/tfobz/vokabeltrainer/gui/CreateLernkartei.java
+++ src/net/tfobz/vokabeltrainer/gui/CreateLernkartei.java
@@ -1,10 +1,22 @@
 package net.tfobz.vokabeltrainer.gui;
 
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CreateLernkartei extends JPanel {
 	public CreateLernkartei() {
-		this.add(new JLabel("Create Lernkartei"));
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		this.add(Box.createRigidArea(new Dimension(0,30)));
+		
+		JLabel titel = new JLabel("Create Lernkartei");
+		titel.setAlignmentX(CENTER_ALIGNMENT);
+		titel.setFont(new Font(titel.getFont().getFontName(),Font.PLAIN, 40));
+		this.add(titel);
 	}
 }
