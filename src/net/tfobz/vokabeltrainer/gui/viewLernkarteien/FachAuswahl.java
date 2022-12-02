bebd9e8a5757c71b2c1d6f3f75f357f40c1d321f
RaphaelSiller <raphael.siller123@gmail.com>
2022-12-02 17:58:18 +0100

    some small improvements to FachAuswahl.

--- src/net/tfobz/vokabeltrainer/gui/viewLernkarteien/FachAuswahl.java
+++ src/net/tfobz/vokabeltrainer/gui/viewLernkarteien/FachAuswahl.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -19,7 +20,13 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import net.tfobz.vokabeltrainer.gui.StartVokabeltrainer;
 import net.tfobz.vokabeltrainer.model.Fach;
@@ -31,29 +38,67 @@ public class FachAuswahl extends JDialog {
 	private static final long serialVersionUID = 3567286090716742665L;
 	
 	public FachAuswahl(Lernkartei kartei, StartVokabeltrainer startVokabeltrainer) {
+		Box box= new Box(BoxLayout.Y_AXIS);
+		
+		
 		this.setModal(true);
 		this.setTitle("Vokabeltrainer - Fachauswahl");
 		this.setMinimumSize(new Dimension(500, 500));
 		this.setLocationRelativeTo(null); //Öffne in der Bildschirmmitte :3
 		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		Box box= new Box(BoxLayout.Y_AXIS);
 		
-//		ArrayList<JPanel> panels = new ArrayList<JPanel>();
+
 		ArrayList<Fach> faecher = (ArrayList<Fach>) VokabeltrainerDB.getFaecher(kartei.getNummer());
 		
-		//Für jedes Fach ein JPanel hinzufuegen
-//		for (int i = 0; i < faecher.size(); i++)
-//		panels.add(new JPanel());
-		box.add(new JLabel("Test"));
+		JButton zuruecksetzen = new JButton("Zurücksetzen!");
+		zuruecksetzen.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(JOptionPane.YES_OPTION == JOptionPane.showConfirmDialog(null, "Wollen Sie wirklich alle Karten in das erste Fach verschieben?", "Zurücksetzen?", JOptionPane.YES_NO_OPTION)) {
+					VokabeltrainerDB.getFaecher(kartei.getNummer()).forEach((fach)->{
+						VokabeltrainerDB.getKarten(fach.getNummer()).forEach((karte)->{
+							VokabeltrainerDB.setKarteFalsch(karte);
+						});
+					});
+					JOptionPane.showMessageDialog(null, "Alle Karten wurden ins erste Fach verschoben", "Erfolg!", JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+		});
+		
+		
+//		box.add(zuruecksetzen);
+		JPanel titlePanel = new JPanel();
+		titlePanel.setLayout(new GridLayout(1, 1));
+		titlePanel.setBorder(BorderFactory.createTitledBorder (BorderFactory.createEtchedBorder (),
+                "",
+                TitledBorder.CENTER,
+                TitledBorder.TOP));
+		titlePanel.setMaximumSize(new Dimension(400, 70));
+		titlePanel.add(new JLabel("Gelernt am", JLabel.CENTER));
+		titlePanel.add(new JLabel("Erinnerung am", JLabel.CENTER));
+		titlePanel.add(new JLabel("", JLabel.CENTER));
+		titlePanel.add(zuruecksetzen);
+		titlePanel.setMaximumSize(new Dimension(450, 50));
+		box.add(titlePanel);
+		
+		box.add(Box.createVerticalStrut(25));
+		
 		faecher.forEach((fach)->{
 			JPanel newPanel = new JPanel();
-			newPanel.setLayout(new GridLayout(1, 4));
-			newPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-			newPanel.setMaximumSize(new Dimension(400, 50));
+			newPanel.setLayout(new GridLayout(1, 1));
 			
-			JLabel beschreibung = new JLabel(fach.getBeschreibung());
-			beschreibung.setHorizontalAlignment(JLabel.CENTER);
-			newPanel.add(beschreibung);
+			//Wenn Fach Faellig ist, wird der Rand rot gefaebt;
+			Boolean isFaellig = fach.getGelerntAm() != null ? fach.getErinnerungFaellig(): false;
+			newPanel.setBorder(BorderFactory.createTitledBorder (BorderFactory.createEtchedBorder((isFaellig ? Color.red : null), null),
+	                "Lorem",
+	                TitledBorder.CENTER,
+	                TitledBorder.TOP));
+			newPanel.setMaximumSize(new Dimension(450, 70));
+			
+//			JLabel beschreibung = new JLabel(fach.getBeschreibung());
+//			beschreibung.setHorizontalAlignment(JLabel.CENTER);
+//			newPanel.add(beschreibung);
 			
 			JLabel gelerntAm = new JLabel(fach.getGelerntAm() != null ? fach.getGelerntAmEuropaeischString() : "noch nie");
 			gelerntAm.setHorizontalAlignment(JLabel.CENTER);
@@ -61,14 +106,21 @@ public class FachAuswahl extends JDialog {
 			
 			JLabel erinnerungFaellig = new JLabel();
 			if(fach.getGelerntAm() != null) {
-				LocalDate erinnerung = LocalDate.of(fach.getGelerntAm().getYear(), fach.getGelerntAm().getMonth(), fach.getGelerntAm().getDay());
-				erinnerungFaellig.setText(erinnerung.plusDays(fach.getErinnerungsIntervall()).toString());
+				SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+				Calendar c = Calendar.getInstance();
+				c.setTime(fach.getGelerntAm());
+				c.add(Calendar.DAY_OF_MONTH, fach.getErinnerungsIntervall());
+				erinnerungFaellig.setText(sdf.format(c.getTime()));
 			} else {
 				erinnerungFaellig.setText("jetzt");
 			}
 			
 			erinnerungFaellig.setHorizontalAlignment(JLabel.CENTER);
 			newPanel.add(erinnerungFaellig);
+			
+			JLabel nKarten = new JLabel(VokabeltrainerDB.getKarten(fach.getNummer()).size() + " Karten");
+			nKarten.setHorizontalAlignment(JLabel.CENTER);
+			newPanel.add(nKarten);
 			
 			JButton playButton = new JButton(new ImageIcon("src/net/tfobz/vokabeltrainer/gui/assets/play_arrow.png"));
 			playButton.addActionListener(new ActionListener() {
